92e18037e830a5c60fda31fbc8adb5496dc68fa0
xuhui <xuhui@yoroko.com>
2017-05-12 18:08:22 +0800

    idontknow

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,12 +11,40 @@ type App struct {
 	GorpController
 }
 
+func (c App) getUser(username string) *models.User {
+	users, err := c.Txn.Select(models.User{}, `select * from User where Username = ?`, username)
+	if err != nil {
+		panic(err)
+	}
+	if len(users) == 0 {
+		return nil
+	}
+	return users[0].(*models.User)
+}
+
 func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Login() revel.Result {
-	return c.Render()
+func (c App) Login(username, password string, remember bool) revel.Result {
+	user := c.getUser(username)
+	if user != nil {
+		err := bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(password))
+		if err == nil {
+			c.Session["user"] = username
+			if remember {
+				c.Session.SetDefaultExpiration()
+			} else {
+				c.Session.SetNoExpiration()
+			}
+			c.Flash.Success("Welcome, " + username)
+			return c.Redirect(routes.Blog.Index())
+		}
+	}
+
+	c.Flash.Out["username"] = username
+	c.Flash.Error("Login failed")
+	return c.Redirect(routes.App.Index())
 }
 
 func (c App) Register(user models.User, verifyPassword string) revel.Result {
@@ -40,5 +68,9 @@ func (c App) Register(user models.User, verifyPassword string) revel.Result {
 
 	c.Session["user"] = user.Username
 	c.Flash.Success("Welcome, " + user.Name)
-	return c.Redirect(routes.Blog.Index())
+	return c.Redirect(routes.App.Profile())
+}
+
+func (c App) Profile() revel.Result {
+	return c.Render()
 }
---
+++ app/models/article.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"fmt"
+	//"github.com/revel/revel"
+	//"regexp"
+)
+
+type Review_Article struct {
+	Article			int
+	Review			int
+
+}
+
+type Review struct {
+	Id			int
+	User			int
+	Content			string
+
+}
+
+type Article struct {
+	Id             		int
+	DateCreated             string
+	DateUpdated             string
+	title			string
+	content     		string
+
+}
+
+func (a *Article) String() string {
+	return fmt.Sprintf("Title(%s)", a.title)
+}
+
+func (a *Article) Preview() string {
+	return fmt.Sprint("%s", a.content[100])
+}
+
+//var userRegex = regexp.MustCompile("^\\w*$")
+//
+//func (user *User) Validate(v *revel.Validation) {
+//	v.Check(user.Username,
+//		revel.Required{},
+//		revel.MaxSize{15},
+//		revel.MinSize{4},
+//		revel.Match{userRegex},
+//	)
+//
+//	ValidatePassword(v, user.Password).
+//		Key("user.Password")
+//
+//	v.Check(user.Name,
+//		revel.Required{},
+//		revel.MaxSize{100},
+//	)
+//}
+//
+//func ValidatePassword(v *revel.Validation, password string) *revel.ValidationResult {
+//	return v.Check(password,
+//		revel.Required{},
+//		revel.MaxSize{15},
+//		revel.MinSize{5},
+//	)
+//}
